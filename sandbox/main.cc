0d07671be8730edd2e598b3bd5cd7d17202db8ea
Marius Utheim <marius@b2.thep.lu.se>
2018-08-28 17:42:58 +0200

    test for Upsilons

--- sandbox/main.cc
+++ sandbox/main.cc
@@ -2,10 +2,38 @@
 #include <iostream>
 #include "tests.h"
 
+void singleEvent()
+{
+  Pythia pythia;
+  pythia.readFile("mymain.cmnd");
+  pythia.init();
+
+  pythia.next();
+
+}
+
+void fingerprint()
+{
+  Pythia pythia("../share/Pythia8/xmldoc", false);
+  pythia.readFile("mymain.cmnd");
+  //pythia.readString("Print:quiet = on");
+  pythia.init();
+
+  while (true)
+  {
+    if (!pythia.next()) continue;
+
+    for (int i = 0; i < pythia.event.size(); ++i)
+      if (pythia.event[i].id() == 553)  {
+        cout << pythia.event[pythia.event.size() - 1].p();
+      return;}
+  }
+
+}
 
 int main(int argc, const char *argv[]) {
 
-  test_pT_distributions();
+  fingerprint();
 
   return 0;
 }
