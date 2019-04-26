e527ca58b5b6dc2165d8df3455929efe68aad33c
Marius Utheim <marius@b2.thep.lu.se>
2019-04-26 12:23:23 +0200

    test resonances

--- sandbox/main.cc
+++ sandbox/main.cc
@@ -1,6 +1,6 @@
 #include <chrono>
 #include <iostream>
-#include "tests.h"
+
 #include "Pythia8/ParticleWidths.h"
 #include "Pythia8/LowEnergyHadHad.h"
 #include "Pythia8/LowEnergySigma.h"
@@ -40,44 +40,7 @@ void fingerprint()
   cout << pythia.event[pythia.event.size() - 1].p();
 }
 
-class Test {
-public:
 
-  static int count;
-
-  int val;
-  void speak() { cout << val << endl; }
-
-  Test() : val(++count) { cout << "()"; speak(); }
-  Test(int val) : val(val) { cout << "++"; speak(); }
-
-  Test(const Test& other) : val(other.val) { cout << "!!"; speak(); }
-  Test(Test&& other) : val(other.val) { cout << "&&"; speak(); }
-};
-
-int Test::count = 10;
-
-static Hist plotFunction(string title, double xMin, double xMax, std::function<double(double)> f) {
-  int nBins = 101;
-  double dx = (xMax - xMin) / (nBins - 1);
-  Hist result(title, nBins, xMin - 0.5 * dx, xMax + 0.5 * dx);
-  for (double x = xMin; x <= xMax + 0.5 * dx; x += dx) {
-    cout << x << " --> " << f(x) << endl;
-    result.fill(x, f(x));
-  }
-  cout << endl << endl;
-  return result;
-}
-static Interpolator ppiDiffData(1.9, 3.19642, {
-    0., 0.597966, 1.6208, 2.64363, 3.66647, 4.6893, 5.71213, 6.67697,
-    7.63029, 8.79865, 10.016, 11.3516, 12.4208, 13.3126, 13.984, 14.5885,
-    15.0755, 15.4614, 15.7966, 16.0741, 16.3701, 16.6786, 16.9763,
-    17.2395, 17.4756, 17.7065, 17.9797, 18.2733, 18.5514, 18.7887,
-    18.9995, 19.1861, 19.3658, 19.4813, 19.5585, 19.6249, 19.6775,
-    19.7497, 19.858, 20.0074, 20.1426, 20.2455, 20.3198, 20.3758,
-    20.4241, 20.4542, 20.4502, 20.4389, 20.4559, 20.5093, 20.5774,
-    20.6341
-  });
 
 constexpr double mN = 0.938;
 constexpr double mpi = 0.135;
@@ -108,32 +71,49 @@ int main(int argc, const char *argv[]) {
   LowEnergySigma sigma;
   sigma.initPtr(&pythia.info, &pythia.particleData, &resonance);
 
-  double eLeft = 0.1, eRight = 5.;
-  double mN = pythia.particleData.m0(2212), mL = pythia.particleData.m0(3122);
+  double eLeft = 0., eRight = 2.;
+  double mN = pythia.particleData.m0(2212), mpi = pythia.particleData.m0(111);
+  double dm = 0;//2 * mN + mpi;
 
   Hist sigmaTotal = Hist::plotFunc(
-    [&](double p) { return sigma.sigmaTotal(2212, 3122, pLabInv(mL, mN, p)); }, 
-    "total", 200, eLeft, eRight);
+    [&](double e0) { return sigma.sigmaTotal(111, 111, e0); }, 
+    "total", 300, eLeft, eRight);
 
   Hist sigmaEl = Hist::plotFunc(
-    [&](double p) { return sigma.sigmaPartial(2212, 3122, pLabInv(mL, mN, p), 2); }, 
+    [&](double e0) { return sigma.sigmaPartial(111, 111, e0, 2); }, 
     "elastic", 100, eLeft, eRight);
 
   Hist sigmaDiff = Hist::plotFunc(
-    [&](double p) { return sigma.sigmaPartial(2212, 3122, pLabInv(mL, mN, p), 1); }, 
-    "diffractive", 100, eLeft, eRight);
+    [&](double e0) { return sigma.sigmaPartial(111, 111, e0, 1); }, 
+    "diffractive", 100, eLeft, eRight);  
 
+  Hist sigmaRes = Hist::plotFunc(
+    [&](double e0) { return sigma.sigmaPartial(111, 111, e0, 7); }, 
+    "resonant", 100, eLeft, eRight);
 
+  Hist sigmarho = Hist::plotFunc(
+    [&](double e0) { return sigma.sigmaPartial(111, 111, e0, 113); }, 
+    "rho", 300, eLeft, eRight);
+
+  Hist sigmaf0 = Hist::plotFunc(
+    [&](double e0) { return resonance.getPartialResonanceSigma(111, 111, 9000221, e0); }, 
+    "f_0(500)", 300, eLeft, eRight);
+
+  Hist sigmaf2 = Hist::plotFunc(
+    [&](double e0) { return sigma.sigmaPartial(111, 111, e0, 225); }, 
+    "f_2", 100, eLeft, eRight);
+
+/*
   // PDG cross sections
   Hist sigmaTotalPDG = Hist::plotFunc(
-    [&](double p) { return interpol(LambdapTotal, p); },
+    [&](double e0) { return interpol(ppTotal, pLab(e0 + dm)); },
     "Total (PDG)", 100, eLeft, eRight);
 
   Hist sigmaElPDG = Hist::plotFunc(
-    [&](double p) { return interpol(LambdapElastic, p); },
+    [&](double e0) { return interpol(ppElastic, pLab(e0 + dm)); },
     "Elastic (PDG)", 100, eLeft, eRight
   );
-  
+*/
   
   
   HistPlot plt("myplot");
@@ -141,8 +121,15 @@ int main(int argc, const char *argv[]) {
   plt.add(sigmaTotal, "-");
   plt.add(sigmaEl, "-");
   plt.add(sigmaDiff, "-");
-  plt.add(sigmaTotalPDG, "h");
-  plt.add(sigmaElPDG, "h");
+  plt.add(sigmaRes, "-");
+  plt.add(sigmarho, "-");
+  plt.add(sigmaf0, "-");
+  plt.add(sigmaf2, "-");
+
+  //plt.add(sigmaEl, "-");
+  //plt.add(sigmaDiff, "-");
+  //plt.add(sigmaTotalPDG, "h");
+  //plt.add(sigmaElPDG, "h");
   plt.plot();
 
 }
