9d8cd945124629c60721de0ba961b4f01bb6d9fa
Marius Utheim <marius@b2.thep.lu.se>
2019-06-28 11:26:56 +0200

    clean and comment

--- include/Pythia8/Interpolator.h
+++ include/Pythia8/Interpolator.h
@@ -1,34 +1,49 @@
+// Interpolator.h is a part of the PYTHIA event generator.
+// Copyright (C) 2019 Torbjorn Sjostrand.
+// PYTHIA is licenced under the GNU GPL v2 or later, see COPYING for details.
+// Please respect the MCnet Guidelines, see GUIDELINES for details.
+
+// Header file for the Interpolator class.
+
 #ifndef Pythia8_Interpolator_H
 #define Pythia8_Interpolator_H
 
 #include "PythiaStdlib.h"
-#include <istream>
 
-using namespace Pythia8;
+namespace Pythia8 {
 
-// @TODO comments, also where to put this? 
+//==========================================================================
+// @TBD Maybe move this to Basics.h?
+// Interpolator class.
+// Used to interpolate between values in linearly spaced data.
+
 class Interpolator {
 public:
 
-  Interpolator(double leftIn, double rightIn, vector<double> ysIn);
+  // Constructor.
+  Interpolator(double leftIn, double rightIn, vector<double> ysIn)
+    : leftSave(leftIn), rightSave(rightIn), ysSave(ysIn) { }
 
-  double left()  const { return leftSave; }
-  double right() const { return rightSave; }
-  
-  double dx() const { return (rightSave - leftSave) / (ysSave.size() - 1); } 
-  double x(int j) const { return leftSave + j * dx(); }
+  // Function to get y-values of interpolation data. 
   const vector<double>& data() const { return ysSave; }
 
-  double operator()(double x) const;
+  // x-values are linearly spaced on the interpolation region
+  double left()  const { return leftSave; }
+  double right() const { return rightSave; }
 
-  static const Interpolator Zero;
+  // Operator to get interpolated value at the specified point
+  double operator()(double x) const;
 
 private:
 
+  // Data members
   double leftSave, rightSave;
   vector<double> ysSave;
 
 };
 
+//==========================================================================
 
-#endif
+} // end namespace Pythia8
+
+#endif // Pythia8_Interpolator_H
--- src/Interpolator.cc
+++ src/Interpolator.cc
@@ -1,26 +1,40 @@
+// Interpolator.cc is a part of the PYTHIA event generator.
+// Copyright (C) 2019 Torbjorn Sjostrand.
+// PYTHIA is licenced under the GNU GPL v2 or later, see COPYING for details.
+// Please respect the MCnet Guidelines, see GUIDELINES for details.
+
+// Implementation of the interpolation function used by th eInterpolator class.
 
 #include "Pythia8/Interpolator.h"
 
-const Interpolator Interpolator::Zero(0., 1., { 0., 0. });
+namespace Pythia8 {
 
-Interpolator::Interpolator(double leftIn, double rightIn, vector<double> ysIn)
-  : leftSave(leftIn), rightSave(rightIn), ysSave(ysIn) {
-  if (ysIn.size() <= 1)
-    cout << "WARNING: Interpolator has too few entries! " << ysIn.size() << endl;
-}
+//==========================================================================
+
+// Interpolator class.
+// Used to interpolate between values in linearly spaced data.
+
+//--------------------------------------------------------------------------
+
+// Operator to get interpolated value at the specified point
 
 double Interpolator::operator()(double xIn) const {
-
+  // @TODO Decide what happens if out of range
   double t = (xIn - leftSave) / (rightSave - leftSave);
   int lastIdx = ysSave.size() - 1;
   int j = (int)floor(t * lastIdx);
+  double dx = (rightSave - leftSave) / (ysSave.size() - 1);
 
   if (j < 0)
     return ysSave[0];
   else if (j >= lastIdx)
     return ysSave[lastIdx];
   else {
-    double s = (xIn - this->x(j)) / dx();
+    double s = (xIn - (leftSave + j * dx)) / dx;
     return (1 - s) * ysSave[j] + s * ysSave[j + 1];
   }
 }
+
+//==========================================================================
+
+} // end namespace Pythia8
